e16a056178d29fc7056863798a8f8b78a5408bb5
Sebastian <brommer.sebastian@gmail.com>
2015-11-03 19:22:12 +0100

    start work on ex 2

---
+++ blatt_2/MadGraph5_v1_3_20/ex_2/Source/MODEL/couplings1.f
@@ -0,0 +1,22 @@
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+c      written by the UFO converter
+ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      SUBROUTINE COUP1()
+
+      IMPLICIT NONE
+      DOUBLE PRECISION PI
+      PARAMETER  (PI=3.141592653589793D0)
+
+
+      INCLUDE 'input.inc'
+      INCLUDE 'coupl.inc'
+
+      GC_1 = -(EE*COMPLEXI)/3.000000D+00
+      GC_2 = (2.000000D+00*EE*COMPLEXI)/3.000000D+00
+      GC_3 = -(EE*COMPLEXI)
+      GC_21 = -(CW*EE*COMPLEXI)/(2.000000D+00*SW)
+      GC_22 = (CW*EE*COMPLEXI)/(2.000000D+00*SW)
+      GC_23 = -(EE*COMPLEXI*SW)/(6.000000D+00*CW)
+      GC_24 = (EE*COMPLEXI*SW)/(2.000000D+00*CW)
+      END
