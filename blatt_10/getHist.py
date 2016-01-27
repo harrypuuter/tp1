e2deb2b34dd38440e6cb876db294d0b159da93a9
Sebastian Brommer <brommer.sebastian@gmail.com>
2016-01-27 11:19:49 +0100

    1.6 done

--- blatt_10/getHist.py
+++ blatt_10/getHist.py
@@ -1,7 +1,11 @@
 from ROOT import TFile, TH1F, TH2F,  TTree, gROOT
 
 gROOT.SetBatch(True)
-
-# FIXME Implement your code here. 
-
-
+hist = TH1F('hist','DO Mass',100,0,2.2)
+f = TFile.Open("resultswithqcut.root")
+for event in f.variables:
+	hist.Fill(event.daughter__bo0__cmInvM__bc)
+file = TFile.Open("hist.root","RECREATE")
+hist.Write()
+# FIXME Implement your code here.
+# branch daughter__bo0__cmInvM__bc
